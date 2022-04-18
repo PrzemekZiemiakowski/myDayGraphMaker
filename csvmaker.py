0a3332b97fc828beb62498f79f998c2ae6088643
PrzemekZiemiakowski <przemo1814@gmail.com>
2022-04-19 01:40:50 +0200

    prawie dziala , do zrobienia jeszcze grafy ale csv maker juz w pelni dziala+do zorbienia podsumowanie bo cos pierdzieli czasem

--- csvmaker.py
+++ csvmaker.py
@@ -1,49 +1,80 @@
 import csv
+
+
 class rzeczDan:
-    def __init__(self,start,end,day,catego):
-        self.start=start
-        self.end=end
-        self.day=day
-        self.category=catego
-    def getHours(self,startOf,endOf):
-        return self.endOf-self.startOf
-    def giveinfo(self):
-        print("start",self.start," end=",self.end," day=",self.day," kategory",self.category)
+    def __init__(self, start, end, day, catego):
+        self.start = start
+        self.end = end
+        self.day = day
+        self.category = catego
+
+    def getHours(self):
+        return int(self.end) - int(self.start)
+
+    def Giveinfo(self):
+        print("start", self.start, " end=", self.end, " day=", self.day, " kategory", self.category)
+
     def write(self):
         with open('alive.csv', 'a+') as csvfile:
             csvwriter = csv.writer(csvfile)
-            csvwriter.writerow(self.start,self.end,self.day,self.category)
-kategoria=["Studia stacjonarnie",
-"Siłownia/Praca ",
-"Jedzenie+gotowanie",
-"Transport",
-"Nauka/programowanie",
-"Przyjaciele/ółki",
-"Dziewczyna",
-"Sen",
-"Muzyka/Dj-ka"]
-czasOd=[]
-czasDo=[]
-days=[]
+            tup = (self.start, self.end, self.day, self.category)
+            csvwriter.writerow(tup)
+
+
+kategoria = ["Studia stacjonarnie",
+             "Siłownia/Praca ",
+             "Jedzenie+gotowanie",
+             "Transport",
+             "Nauka/programowanie",
+             "Przyjaciele/ółki",
+             "Dziewczyna",
+             "Sen",
+             "Muzyka/Dj-ka"]
+czasOd = []
+czasDo = []
+days = []
 # ta lista bedzie przechowywac wszystkie posortowane po dniu i godzinie rozpoczecia
-summaryArray=[]
+summaryArray = []
+
 
 def listPrinterwithIndex(kategoria):
-    for number,kat in enumerate(kategoria):
-        print(number," = ",kat)
-def zapytanie():
+    for number, kat in enumerate(kategoria):
+        print(number, " = ", kat)
+
+
+def quitChecker(inputcik):
+    if inputcik == "quit":
+        quit()
+
+
+def zapytanie(i):
     listPrinterwithIndex(kategoria)
-    kat=input("Wybierz kategorie:")
-    odkiedy=input("Podaj od kiedy:")
+    kat = input("Wybierz kategorie:")
+    quitChecker(kat)
+    odkiedy = input("Podaj od kiedy:")
+    quitChecker(odkiedy)
     czasOd.append(odkiedy)
-    dokiedy=input("Podaj od kiedy:")
+    dokiedy = input("Podaj od kiedy:")
     czasDo.append(dokiedy)
-    day=input("Podaj ktorego dnia:")
+    day = input("Podaj ktorego dnia:")
     days.append(day)
-    tere=rzeczDan(odkiedy,dokiedy,day,kat)
-    summaryArray.append(tere)
-    tere.giveinfo()
-    print(summaryArray)
+    summaryArray.append(rzeczDan(odkiedy, dokiedy, day, kat))
+    summaryArray[i].write()
 
 
-zapytanie()
+def odczyt():
+    for obj in summaryArray:
+        obj.Giveinfo()
+        print(obj.start, " end=", obj.end, " day=", obj.day, " kategory", obj.category)
+
+
+def podsumowanieby(categoryOfSearch):
+    temp = 0
+    for obj in summaryArray:
+        if obj.category == "1":
+            print("imin")
+            temp += obj.getHours()
+            print("tererere")
+            print(obj.getHours())
+            print("temp")
+            print(temp)
--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+import keyboard
+import csvmaker
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    i = 0
+    while True:
+        csvmaker.zapytanie(i)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        i = i + 1
+        inputcik=input("chcesz skonczyc")
+        if(inputcik=="quit"):
+            csvmaker.odczyt()
+            csvmaker.podsumowanieby(1)
+            break
